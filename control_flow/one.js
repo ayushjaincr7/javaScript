294eb714778e09a61174036f73048d32f85e8043
ayushjaincr7 <ja7ayush@gmail.com>
2023-10-04 13:55:50 +0530

    if

---
+++ control_flow/one.js
@@ -0,0 +1,55 @@
+// if
+
+// condition to be true to execute
+// if (condition) {
+
+// }
+
+if (false) {
+    // never execute
+}
+
+const isUserLoggedIn = true
+
+if (isUserLoggedIn) {
+
+}
+// <, >, <=, >=, ==, !=, === (check type)
+
+// const score = 200
+
+// if (score > 100){
+//     const power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+
+// console.log(`User power: ${power}`)
+
+const balance = 1000
+
+// if (balance > 500) console.log("test"), console.log("test2");
+
+// if (balance < 500) {
+//     console.log("less than 500");
+// }
+// else if (balance < 750){
+//     console.log("less than 750");
+// }
+// else if (balance < 900){
+//     console.log("less than 900");
+// }
+// else {
+//     console.log("less than 1200");
+// }
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle =false
+const loggedInFromEmail = true
+
+if(userLoggedIn && debitCard && 2==3){
+    console.log("Allow to buy course");
+}
+if(loggedInFromEmail || loggedInFromGoogle){
+    console.log("Allow to buy course");
+}
