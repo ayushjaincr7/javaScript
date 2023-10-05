cf803cd8ad9a6160a7976314c0f0a9494d12a5a4
ayushjaincr7 <ja7ayush@gmail.com>
2023-10-05 10:03:11 +0530

    loops

---
+++ loop/02_two.js
@@ -0,0 +1,17 @@
+// while(condition){
+
+// }
+let index = 0
+while (index <=10) {
+    // console.log(index)
+    index += 2
+}
+
+// do {
+    
+// } while (condition);
+let score = 11
+do {
+    console.log(score)
+    score++
+} while (score <= 10);
